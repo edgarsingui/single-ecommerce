8e08eb0296e6f682d7b93f1346b559aa0e495590
Edgar Singui <edgar@Edgars-MacBook-Pro.local>
2023-04-11 00:43:31 +0100

    “😑”

---
+++ src/main/java/com/single_ecomerce/single_ecomerce/SingleEcomerceApplication.java
@@ -0,0 +1,13 @@
+package com.single_ecomerce.single_ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleEcomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleEcomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/single_ecomerce/single_ecomerce/SingleEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single_ecomerce.single_ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
