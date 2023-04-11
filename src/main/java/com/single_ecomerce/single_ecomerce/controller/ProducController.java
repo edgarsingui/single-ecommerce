4c61068f742bc357e4a8b6de602e8d4bf9f4243c
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 11:27:13 +0100

    Adding products controller

---
+++ src/main/java/com/single_ecomerce/single_ecomerce/controller/ProducController.java
@@ -0,0 +1,12 @@
+package com.single_ecomerce.single_ecomerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class ProducController {
+
+    @RequestMapping("/products")
+    public String all(){
+        return  "products/products";
+    }
+}
