4d8d4155d695b2b44830f882e2790a1e554eff71
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 15:48:54 +0100

    ❇️ Creating Category Model

---
+++ src/main/java/com/single_ecomerce/single_ecomerce/models/Category.java
@@ -0,0 +1,32 @@
+package com.single_ecomerce.single_ecomerce.models;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    // getters e setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
