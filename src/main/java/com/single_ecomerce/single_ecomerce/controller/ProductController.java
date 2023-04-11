cad5301e57195d7acb4ad63106242cf0845719b3
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 15:09:27 +0100

    🔄Fix class name

--- src/main/java/com/single_ecomerce/single_ecomerce/controller/ProductController.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/controller/ProductController.java
@@ -3,7 +3,7 @@ package com.single_ecomerce.single_ecomerce.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-public class ProducController {
+public class ProductController {
 
     @RequestMapping("/products")
     public String all(){
