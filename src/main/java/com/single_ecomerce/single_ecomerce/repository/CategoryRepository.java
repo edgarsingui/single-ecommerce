5d69c0450072ebd2b26d3b5f0efe6d05e42e3f87
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 22:35:47 +0100

    ❇️ Add filter by slug method in CategoryRepository

--- src/main/java/com/single_ecomerce/single_ecomerce/repository/CategoryRepository.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.single_ecomerce.single_ecomerce.repository;
 import com.single_ecomerce.single_ecomerce.models.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+    List<Category> findByslug(String slug);
 }
