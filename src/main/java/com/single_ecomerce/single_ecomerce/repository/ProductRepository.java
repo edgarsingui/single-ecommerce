17fdeab0748d03a580955f603edc63bd65657281
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 20:57:16 +0100

    ❇️ Add feature search method in ProductRepository

--- src/main/java/com/single_ecomerce/single_ecomerce/repository/ProductRepository.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/repository/ProductRepository.java
@@ -2,5 +2,8 @@ package com.single_ecomerce.single_ecomerce.repository;
 import com.single_ecomerce.single_ecomerce.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product,Long> {
+    List<Product> findByFeatured(Boolean featured);
 }
