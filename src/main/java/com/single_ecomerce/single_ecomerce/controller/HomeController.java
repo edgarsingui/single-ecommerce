6c2301d3cdfcd041dcddcb1a5f7e745cd8ebae82
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 20:35:05 +0100

    ❇️ Geting products in db

--- src/main/java/com/single_ecomerce/single_ecomerce/controller/HomeController.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/controller/HomeController.java
@@ -1,16 +1,22 @@
 package com.single_ecomerce.single_ecomerce.controller;
 
+import com.single_ecomerce.single_ecomerce.models.Product;
+import com.single_ecomerce.single_ecomerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+    @Autowired
+    private ProductRepository productRepository;
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("dir","home/index");
         model.addAttribute("title","Inicio");
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("produtos",products);
         return  "template";
     }
 }
