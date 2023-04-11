85b464f0cd53b9f1324a94e383ebc01ad2d24917
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 22:32:52 +0100

    ❇️ Getting categories

--- src/main/java/com/single_ecomerce/single_ecomerce/controller/HomeController.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.single_ecomerce.single_ecomerce.controller;
 
+import com.single_ecomerce.single_ecomerce.models.Category;
 import com.single_ecomerce.single_ecomerce.models.Product;
+import com.single_ecomerce.single_ecomerce.repository.CategoryRepository;
 import com.single_ecomerce.single_ecomerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("dir","home/index");
         model.addAttribute("title","Inicio");
         Iterable<Product> products = productRepository.findTop5ByFeatured(true);
+        Iterable<Category> categories = categoryRepository.findAll();
         model.addAttribute("produtos",products);
+        model.addAttribute("categorias",categories);
         return  "template";
     }
 }
