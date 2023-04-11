27acf59a0d5b8a57e72b865d28efd825e6018d86
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 12:43:22 +0100

    ❇️ Creating class for consts.

---
+++ src/main/java/com/single_ecomerce/single_ecomerce/util/Consts.java
@@ -0,0 +1,9 @@
+package com.single_ecomerce.single_ecomerce.util;
+
+public class Consts {
+
+     public static final String dataBaseUrl = "jdbc:mysql://localhost:3306/singlevendor";
+     public static final String databaseUser = "root";
+     public static final String databasePwd = "";
+
+}
