0084b26c5126ba60a59eec51cd714ea917d1b0c6
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 12:44:46 +0100

    ❇️ Creating class for Connection

---
+++ src/main/java/com/single_ecomerce/single_ecomerce/database/Connection.java
@@ -0,0 +1,37 @@
+package com.single_ecomerce.single_ecomerce.database;
+
+import com.single_ecomerce.single_ecomerce.util.Consts;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Connection {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl(Consts.dataBaseUrl);
+        driverManagerDataSource.setUsername(Consts.databaseUser);
+        driverManagerDataSource.setPassword(Consts.databasePwd);
+        return  driverManagerDataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        adapter.setPrepareConnection(true);
+        return  adapter;
+    }
+}
+
