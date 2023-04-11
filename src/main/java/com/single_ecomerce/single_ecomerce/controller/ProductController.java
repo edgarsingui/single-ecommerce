6b5ba1899abf6a6bd926d2c7e6f7d1f7acaa7b43
Edgar Singui <edgarsingui@gmail.com>
2023-04-11 22:40:09 +0100

    ❇️ Getting all products

--- src/main/java/com/single_ecomerce/single_ecomerce/controller/ProductController.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/controller/ProductController.java
@@ -1,12 +1,22 @@
 package com.single_ecomerce.single_ecomerce.controller;
 
+import com.single_ecomerce.single_ecomerce.models.Product;
+import com.single_ecomerce.single_ecomerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
     @RequestMapping("/products")
-    public String all(){
-        return  "products/products";
+    public String all(Model model){
+        model.addAttribute("dir","products/products");
+        model.addAttribute("title","Produtos");
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("produtos",products);
+        return  "template";
     }
 }
