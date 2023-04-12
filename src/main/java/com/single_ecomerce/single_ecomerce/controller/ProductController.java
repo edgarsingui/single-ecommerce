ffcde774c3abc13c48a88a6abbbd1bcd46298b70
Edgar Singui <edgarsingui@gmail.com>
2023-04-12 08:29:01 +0100

    ❇️ Showing Cateogires in products page

--- src/main/java/com/single_ecomerce/single_ecomerce/controller/ProductController.java
+++ src/main/java/com/single_ecomerce/single_ecomerce/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.single_ecomerce.single_ecomerce.controller;
 
+import com.single_ecomerce.single_ecomerce.models.Category;
 import com.single_ecomerce.single_ecomerce.models.Product;
+import com.single_ecomerce.single_ecomerce.repository.CategoryRepository;
 import com.single_ecomerce.single_ecomerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +13,16 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
     @RequestMapping("/products")
     public String all(Model model){
         model.addAttribute("dir","products/products");
         model.addAttribute("title","Produtos");
         Iterable<Product> products = productRepository.findAll();
         model.addAttribute("produtos",products);
+        Iterable<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categorias",categories);
         return  "template";
     }
 }
